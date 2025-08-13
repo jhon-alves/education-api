73e7783f5d5e406057b467ea381ca92b766fce47
jhon-alves <jonatas.a2012@hotmail.com>
2025-08-13 15:00:41 -0300

    Create get-course-by-id.ts

---
+++ src/routes/courses/get-course-by-id.ts
@@ -0,0 +1,35 @@
+import z from 'zod'
+import { eq } from 'drizzle-orm'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { db } from '../../database/client.ts'
+import { courses } from '../../database/schema.ts'
+import { courseSchema } from '../../schemas/course.schema.ts'
+
+export const getCourseByIdRoute: FastifyPluginAsyncZod = async (server) => {
+  server.get('/courses/:id', {
+    schema: {
+      tags: ['courses'],
+      summary: 'Get course by ID',
+      params: z.object({
+        id: z.uuid(),
+      }),
+      response: {
+        200: z.object({ course: courseSchema }),
+        404: z.null().describe('Course not found'),
+      },
+    },
+  }, async (request, reply) => {
+    const courseId = request.params.id
+  
+    const result = await db
+      .select()
+      .from(courses)
+      .where(eq(courses.id, courseId))
+  
+    if (result.length > 0) {
+      return { course: result[0] }
+    }
+  
+    return reply.status(404).send()
+  })
+}
